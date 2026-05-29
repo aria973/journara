3067945cb7d3f91aa6e289b20e4f961f0da276c3
Aria <108920295+aria973@users.noreply.github.com>
2026-05-29 23:20:34 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE = "journara-pro-v1";
+const CACHE = "journara-pro-v2";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
+  "./db.js",
   "./manifest.webmanifest"
 ];
 
 self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 
 self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(r=>r || fetch(e.request))
   );
 });

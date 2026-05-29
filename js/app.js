2159addb016065b84f01d2b3f20ebcdc1854adca
Aria <108920295+aria973@users.noreply.github.com>
2026-05-29 17:42:59 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,13 +4,159 @@ import {
   getAllJournals
 } from "./db.js";
 
+import {
+  uid,
+  formatDate
+} from "./storage.js";
+
+import {
+  updateMood
+} from "./ui.js";
+
+const moodSlider =
+document.getElementById(
+  "moodSlider"
+);
+
+const openTodayBtn =
+document.getElementById(
+  "openTodayBtn"
+);
+
+const editorModal =
+document.getElementById(
+  "editorModal"
+);
+
+const saveEntryBtn =
+document.getElementById(
+  "saveEntryBtn"
+);
+
+const entryTitle =
+document.getElementById(
+  "entryTitle"
+);
+
+const entryBody =
+document.getElementById(
+  "entryBody"
+);
+
+const journalList =
+document.getElementById(
+  "journalList"
+);
+
+moodSlider.addEventListener(
+"input",
+()=>{
+
+  updateMood(
+    moodSlider.value
+  );
+
+});
+
+openTodayBtn.addEventListener(
+"click",
+()=>{
+
+  editorModal.showModal();
+
+});
+
+saveEntryBtn.addEventListener(
+"click",
+async ()=>{
+
+  const data = {
+
+    id:uid(),
+
+    title:
+      entryTitle.value,
+
+    body:
+      entryBody.value,
+
+    mood:
+      Number(
+        moodSlider.value
+      ),
+
+    date:
+      Date.now()
+
+  };
+
+  await saveJournal(data);
+
+  renderJournals();
+
+  editorModal.close();
+
+});
+
+async function renderJournals(){
+
+  const journals =
+  await getAllJournals();
+
+  journalList.innerHTML = "";
+
+  journals.reverse().forEach(
+  journal=>{
+
+    const item =
+    document.createElement(
+      "button"
+    );
+
+    item.className =
+    "journal-card glass";
+
+    item.innerHTML = `
+
+      <div class="journal-date">
+        ${
+          formatDate(
+            journal.date
+          )
+        }
+      </div>
+
+      <div class="journal-title">
+        ${journal.title}
+      </div>
+
+    `;
+
+    journalList.appendChild(
+      item
+    );
+
+  });
+
+}
+
 async function bootstrap(){
 
   await initDB();
 
-  console.log("DB READY");
+  await renderJournals();
 
-  initUI();
+  updateMood(3);
+
+  if(
+    "serviceWorker"
+    in navigator
+  ){
+
+    navigator.serviceWorker
+    .register("./sw.js");
+
+  }
 
 }
 
